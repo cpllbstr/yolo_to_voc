8660f9db36083bf9cdf0b984896e492863482315
lbstr <golubitskiy_savv@mail.ru>
2020-02-10 13:21:42 +0300

    converter ready

--- ytov.py
+++ ytov.py
@@ -1,51 +1,87 @@
-import mako
+from mako import template
 import sys
-from os import listdir
-from os.path import isfile, join
+import argparse
+import imagesize
+from os import listdir, mkdir
+from os.path import isfile, split, abspath
 
+parser = argparse.ArgumentParser(description='Convert YOLO dataset to Pascal VOC.')
+parser.add_argument('--data', metavar='/yolo/dataset', type=str,
+                    help='path to images and notations')
+parser.add_argument('--notes', metavar='/yolo/notes', type=str,
+                    help='if notes are separate to images, then use this argument to define path to notes')
+parser.add_argument('--out', metavar='/out/dir', type=str,
+                    help='output directory')
 
 names = []
 
 class Detection():
-    __slots__ = (
-        "clss",
-        "name",
-        "x",
-        "y",
-        "w", 
-        "h"
-    )
-    def __init__(self, yolo_line):
-        spl = yolo_line.split(" ")
-        self.clss = spl[0]
-        self.name = names[int(spl[0])]
-        self.x = spl[0]
-        self.y = spl[1]
-        self.w = spl[2]
-        self.h = spl[3]
+    def __init__(self, path, width, height):
+        folder, self.filename = split(path)
+        self.folder = split(folder)[1]
+        self.path = abspath(path)
+        self.width = width
+        self.height = height
+        self.folder = folder
+        self.path = path
+        self.objects = []
 
+    def add_object(self, yolo_line):
+        self.objects.append(Object(yolo_line, self.width, self.height))
+       
+
+class Object:
+    def __init__(self, yolo_line, img_width, img_height):
+        self.name, x, y, w, h = yolo_line.split(" ")  
+        self.xmin = (float(x)-float(w)/2)*img_width
+        self.xmax = (float(x)+float(w)/2)*img_width
+        self.ymin = (float(y)-float(h)/2)*img_height
+        self.ymax = (float(y)+float(h)/2)*img_height
 
 if __name__ == "__main__":
-    # if len(sys.argv) < 4:
-        # print("Usage: python ytov.py /path/to/yolo/data /path/to/yolo/names /output/path")
-    yolo_data_folder = sys.argv[1]
-    # yolo_names_path = sys.argv[2]
-    names = open(sys.argv[2]).readlines()
-    # output_folder = sys.argv[3]
+    args = parser.parse_args()
+    yolo_data_folder = args.data
+    yolo_txt_p = args.notes if args.notes is not None else args.data
+    out_path = args.out if args.out is not None else "./output"
+    try:
+        mkdir(out_path)
+    except Exception as e:
+        print ("Creation of the directory %s failed" % out_path)
+        print ("Error:", e)
 
+    yolo_img = [img for img in listdir(yolo_data_folder) if (img.split(".")[1] == "jpg" or img.split(".")[1] == "png" or img.split(".")[1] == "jpeg" )]
 
-
-    yolo_files = [f for f in listdir(yolo_data_folder) if isfile(join(yolo_data_folder, f))]
-
-    res = {}
-
-    for filename in yolo_files:
-        file = open(yolo_data_folder +"/"+ filename)
-        content = file.readlines()
-        dets = []
+    res = []
+    for filename in yolo_img:
+        txt_path = yolo_txt_p +"/"+ filename.split(".")[0]+".txt"
+        img_path = yolo_data_folder+"/"+filename
+        try:
+            note = open(txt_path)
+        except Exception as e:
+            print("Error:", e)
+            continue
+        content = note.readlines()
+        w, h = imagesize.get(img_path)
+        _, folder = split(yolo_data_folder)
+        dets = Detection(img_path , w, h)
         for line in content:
-            dets.append(Detection(line))
-        res[filename] = dets
+            dets.add_object(line)
+        res.append(dets)
+    
+    i = 0
+    allr= len(res)
+    for r in res:
+        tmp = template.Template(filename="voc_template.xml")
+        ready = int(25*i/allr)
+        print("\r [" ,"="*(ready+1), " "*(25-ready-1), end="]")
+        i+=1
+        f = open(out_path + "/" + r.filename.split(".")[0] + ".xml", "w+")
+        f.write(tmp.render_unicode(data=r))
+        f.close()
+
+    print()
+            
+
     
 
 
